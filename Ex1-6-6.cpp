6e06e2c13cabd27ac75fb066cfa6513bc63f2127
LingNinghuang <138959332+LingNinghuang@users.noreply.github.com>
2023-07-10 12:25:22 +0800

    Create Ex1-6-6.cpp

---
+++ Ex1-6-6.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int studentid = 11;
+    int studentage = 24;
+    float studentfee = 87.43;
+    char studentgrade = 'A';
+    printf("student id:%d\n", studentid);
+    printf("student age:%d\n", studentage);
+    printf("student fee:%f\n", studentfee);
+    printf("student grade:%c", studentgrade);
+    return 0;
+}
